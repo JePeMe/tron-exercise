f01855db38485ff2e08ee7032401ce51d8860f92
Julia Friesel <Julia.Friesel@softgarden.de>
2016-05-13 14:31:49 +0200

    add button to create game

--- client.js
+++ client.js
@@ -6,13 +6,22 @@ document.addEventListener('DOMContentLoaded', function() {
 
     connection.onopen = function() {
         console.log('connection established');
+        initLobby(connection);
     };
+});
+
+
+
+function initLobby(connection) {
+    document.getElementById('createButton')
+        .addEventListener('click', function() {
+            connection.send(JSON.stringify({
+                id: 'thebestgameofalltimesever',
+                type: 'create'
+            }));
+        });
 
     connection.onmessage = function(event) {
         console.log(event.data);
-        connection.send(JSON.stringify({
-            id: 'thebestgameofalltimesever',
-            type: 'create'
-        }));
     };
-});
+}
